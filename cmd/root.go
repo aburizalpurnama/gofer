fc8cca6112448b6ed37d09962d5ac681464880cb
rizal purnama <purnamar9@gmail.com>
2025-11-08 09:23:14 +0700

    chore: add root command

---
+++ cmd/root.go
@@ -0,0 +1,41 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "gofer",
+	Short: "A blazingly fast, DDL-first scaffolder for REST APIs",
+	Long:  `Gofer is a command-line tool that accelerates the development of RESTful APIs. It works by reading your existing SQL database schema (DDL) and generating a complete, layered application skeleton based on your tables.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gofer.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "github.com/aburizalpurnama/gofer/cmd"
+
+func main() {
+	cmd.Execute()
+}
